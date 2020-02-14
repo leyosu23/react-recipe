b745bae80209707016c909899274de95eddb7cdc
leyosu23 <52172195+leyosu23@users.noreply.github.com>
2020-02-14 05:57:39 -0500

    Updates

---
+++ precache-manifest.d250cbef91aa39223bb2517431221b1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d0341fe9b0e291a4bc11a908c23971f",
+    "url": "/react-recipe/index.html"
+  },
+  {
+    "revision": "c12bb3d3430ab1ed4977",
+    "url": "/react-recipe/static/css/main.973e7c65.chunk.css"
+  },
+  {
+    "revision": "4e523411f7dc9aa1d4b2",
+    "url": "/react-recipe/static/js/2.ca224955.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-recipe/static/js/2.ca224955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c12bb3d3430ab1ed4977",
+    "url": "/react-recipe/static/js/main.4fd6a45c.chunk.js"
+  },
+  {
+    "revision": "255b6cc2ec3341164884",
+    "url": "/react-recipe/static/js/runtime-main.a5d71551.js"
+  }
+]);
